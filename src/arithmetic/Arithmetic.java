0f28a18451ca2ebfedf91ad6775f08310baf4bc4
MDGuzma <degumark@sheridancollege.ca>
2024-10-15 18:16:05 -0400

    Arithmetic following OO principles

--- src/arithmetic/Arithmetic.java
+++ src/arithmetic/Arithmetic.java
@@ -21,16 +21,25 @@ public class Arithmetic
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
-    {
-       
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
         ArithmeticBase r= new ArithmeticBase();
-        Scanner in= new Scanner(System.in);
-        int n= in.nextInt();
-        int m= in.nextInt();
-        double result = r.calculate(m,n);
-        System.out.println("result :" +result); 
+
+        // Input for operator and numbers
+        System.out.println("Enter arithmetic operation to Perform: ");
+        String inputOperation = sc.next().toUpperCase();
+        
+        System.out.println("Enter first number:");
+        double firstNum = sc.nextDouble();
+        System.out.println("Enter second number:");
+        double secondNum = sc.nextDouble();
+  
+        //operation is assigned
+        ArithmeticBase.Operation operation = ArithmeticBase.Operation.valueOf(inputOperation);
+
+        // Perform calculations and display the result
+        double result = r.calculate(operation, firstNum, secondNum);
+        System.out.println("result :" + result); 
     
     }
 }
-
--- src/arithmetic/ArithmeticBase.java
+++ src/arithmetic/ArithmeticBase.java
@@ -5,31 +5,26 @@
  */
 package arithmetic;
 
-import java.util.Scanner;
-
 /** This class takes String input plus,minus,divide and times
  * from user and execute the arithmetic operation
  * change the code to use enum instead String and mention the advantage of enum.
  * @author sivagamasrinivasan
  * 
  */
-public class ArithmeticBase 
-{
- public double x,y;
-    double calculate(double x, double y) 
-        {
-        Scanner sc =new Scanner(System.in);
-        System.out.println("Enter arithmetic operation to Perform: ");
-        String s= sc.next();
-        switch (s.toUpperCase()) 
-        {
-            case "PLUS":
+public class ArithmeticBase {
+    
+    public enum Operation {PLUS, MINUS, TIMES, DIVIDE} 
+    
+    // Method for calculations once input is recieved
+    public double calculate(Operation operation, double x, double y) {
+        switch (operation) {
+            case PLUS:
                 return x + y;
-            case "MINUS":
+            case MINUS:
                 return x - y;
-            case "TIMES":
+            case TIMES:
                 return x * y;
-            case "DIVIDE":
+            case DIVIDE:
                 return x / y;
             default:
                 throw new AssertionError("Unknown operations " + this);
